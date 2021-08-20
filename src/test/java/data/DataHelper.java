11049653dba6fbc2b13e416bfbd562e3cfaa1089
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-08-20 12:00:45 +0300

    datahelper creating started

---
+++ src/test/java/data/DataHelper.java
@@ -0,0 +1,23 @@
+package data;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class DataHelper {
+
+    public static String generateCity(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.address().cityName();
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+}
