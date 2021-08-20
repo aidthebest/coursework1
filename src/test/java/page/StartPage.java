820eee3726283e23713a5315f6ef94eb4115c8c8
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-08-20 11:18:39 +0300

    properties for DBs added

--- src/test/java/page/StartPage.java
+++ src/test/java/page/StartPage.java
@@ -1,4 +1,21 @@
 package page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+
 public class StartPage {
+    private SelenideElement heading = $("[class=\"App_appContainer__3jRx1\"]");
+    private SelenideElement buyButton = $(withText("Купить"));
+    private SelenideElement buyWithCreditButton = $(withText("Купить в кредит"));
+
+    public StartPage () {heading.shouldBe(Condition.visible);}
+    public void buyButtonClick () {
+        buyButton.click();
+    }
+    public void buyWithCreditButtonClick () {
+        buyWithCreditButton.click();
+    }
 }
--- src/test/java/test/PurchaseTest.java
+++ src/test/java/test/PurchaseTest.java
@@ -1,4 +1,21 @@
 package test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import page.StartPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class PurchaseTest {
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:8080");
+    }
+
+    @Test
+    public void buyTest () {
+        var startPage = new StartPage();
+        startPage.buyButtonClick();
+        startPage.buyWithCreditButtonClick();
+    }
 }
